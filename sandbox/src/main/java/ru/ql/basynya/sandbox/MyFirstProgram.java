83f655aa0d1b1887703c964cef9deb10f0332c03
Andarkis <Standartis@gmail.com>
2018-06-11 19:04:11 +0300

    Созданы классы Rectangle и Square

--- sandbox/src/main/java/ru/ql/basynya/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/ql/basynya/sandbox/MyFirstProgram.java
@@ -4,23 +4,22 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     hello("Andrey");
-    double l = 3;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-    double a = 2;
-    double b = 5;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Square s = new Square(3);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+    Rectangle r = new Rectangle(2,5);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello " + somebody + "!");
   }
 
-  public static double area(double l) {
-    return l * l;
+  public static double area(Square s) {
+    return s.l * s.l;
   }
 
-  public static double area(double a, double b) {
-    return a * b;
+  public static double area(Rectangle r) {
+    return r.a * r.b;
   }
 
 }
