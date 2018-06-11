1e46878952d99c90de4fead0ec678ddf205971eb
Andarkis <Standartis@gmail.com>
2018-06-11 21:20:34 +0300

    Добавлены примеры вызова метода distance для объектов класса Point

--- sandbox/src/main/java/ru/ql/basynya/homework/MainHomeworkClass.java
+++ sandbox/src/main/java/ru/ql/basynya/homework/MainHomeworkClass.java
@@ -2,9 +2,13 @@ package ru.ql.basynya.homework;
 
 public class MainHomeworkClass {
   public static void main(String[] args) {
-    Point a = new Point(1,0);
-    Point b = new Point(4,4);
-    System.out.println("Растояние между точками (" + a.x + ", " + a.y +") и (" + b.x + ", " + b.y + ") = " + distance(a,b));
+    Point a = new Point(0,0);
+    Point b = new Point(3,4);
+    Point c = new Point(4,4);
+    //System.out.println("Растояние между точками (" + a.x + ", " + a.y +") и (" + b.x + ", " + b.y + ") = " + distance(a,b));
+    System.out.println("Растояние между точками (" + a.x + ", " + a.y +") и (" + b.x + ", " + b.y + ") = " + a.distance(b));
+    System.out.println("Растояние между точками (" + a.x + ", " + a.y +") и (" + c.x + ", " + c.y + ") = " + a.distance(c));
+    System.out.println("Растояние между точками (" + b.x + ", " + b.y +") и (" + c.x + ", " + c.y + ") = " + b.distance(c));
   }
 
   public static double distance(Point p1, Point p2) {
