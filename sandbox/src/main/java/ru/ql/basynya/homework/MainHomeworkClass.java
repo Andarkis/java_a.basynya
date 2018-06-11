77adf6e81b26557d919cfa3d77ff80c59f2e2afe
Andarkis <Standartis@gmail.com>
2018-06-11 20:54:25 +0300

    Создана функция distance, которая возвращает расстояние между двумя точками

--- sandbox/src/main/java/ru/ql/basynya/homework/MainHomeworkClass.java
+++ sandbox/src/main/java/ru/ql/basynya/homework/MainHomeworkClass.java
@@ -4,4 +4,8 @@ public class MainHomeworkClass {
   public static void main(String[] args) {
 
   }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+  }
 }
