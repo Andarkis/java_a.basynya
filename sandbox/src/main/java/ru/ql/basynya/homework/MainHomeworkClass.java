9841083f40259384c7e580907338902743bd49b0
Andarkis <Standartis@gmail.com>
2018-06-11 21:00:38 +0300

    Добавлен пример вызова функции distance

--- sandbox/src/main/java/ru/ql/basynya/homework/MainHomeworkClass.java
+++ sandbox/src/main/java/ru/ql/basynya/homework/MainHomeworkClass.java
@@ -2,7 +2,9 @@ package ru.ql.basynya.homework;
 
 public class MainHomeworkClass {
   public static void main(String[] args) {
-
+    Point a = new Point(1,0);
+    Point b = new Point(4,4);
+    System.out.println("Растояние между точками (" + a.x + ", " + a.y +") и (" + b.x + ", " + b.y + ") = " + distance(a,b));
   }
 
   public static double distance(Point p1, Point p2) {
