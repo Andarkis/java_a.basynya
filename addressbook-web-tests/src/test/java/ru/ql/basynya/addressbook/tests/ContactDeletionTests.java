d4b9f303dbd977cc409889d34ab7ff07df8a5868
Andarkis <Standartis@gmail.com>
2018-06-18 01:25:40 +0300

    Создан класс-помощник NavigationHelper
    В него перенесены вспомогательные методы для работы с навигацией

--- addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/tests/ContactDeletionTests.java
@@ -6,7 +6,7 @@ public class ContactDeletionTests extends TestBase {
 
   @Test
   public void testContactDeletion() {
-    app.gotoHomePage();
+    app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().selectContact();
     app.getContactHelper().deleteSelectedContacts();
     app.getContactHelper().acceptContactDeletionAlert();
