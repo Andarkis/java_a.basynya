80e6c37e265f4efc0d51c5338581f8a0c7bc6851
Andarkis <Standartis@gmail.com>
2018-06-24 22:47:16 +0300

    Изменен testContactModification
    1. Создание объекта с данными для заполнения, вынесено из вызова.
    2. Добавлено значение null для group в  данные для заполнения.
    3. В метод  fillContactForm передано значение false для параметра creation.

--- addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/tests/ContactModificationTests.java
@@ -7,9 +7,22 @@ public class ContactModificationTests extends TestBase {
 
   @Test
   public void testContactModification() {
+    ContactData contactData = new ContactData(
+            null,
+            null,
+            null,
+            "t55555",
+            null,
+            "t88000000000",
+            "t1@example.com",
+            "t2@example.com",
+            "t3@ecample.com",
+            null
+    );
+
     app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData(null, null, null, "t55555", null, "t88000000000", "t1@example.com", "t2@example.com", "t3@ecample.com"));
+    app.getContactHelper().fillContactForm(contactData, false);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
   }
