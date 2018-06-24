41a08e88dc7681ff30fc34389f9af900051320dc
Andarkis <Standartis@gmail.com>
2018-06-24 20:22:16 +0300

    Добавлена возможность оставлять дефолтное значение поля

--- addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase{
   @Test
   public void testContactCreation() {
     app.getContactHelper().initContactCreation();
-    app.getContactHelper().fillContactForm(new ContactData("John", "Doe", "test1", "55555", "88005553535", "88000000000", "test1@example.com", "test2@example.com", "test3@ecample.com"));
+    app.getContactHelper().fillContactForm(new ContactData("John", null, "test1", "55555", "88005553535", "88000000000", "test1@example.com", "test2@example.com", "test3@ecample.com"));
     app.getContactHelper().submitContactCreation();
     app.getContactHelper().returnToHomePage();
   }
--- addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/tests/ContactModificationTests.java
@@ -9,7 +9,7 @@ public class ContactModificationTests extends TestBase {
   public void testContactModification() {
     app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("John 2", "Doe 2", "test1", "t55555", "t88005553535", "t88000000000", "t1@example.com", "t2@example.com", "t3@ecample.com"));
+    app.getContactHelper().fillContactForm(new ContactData(null, null, null, "t55555", null, "t88000000000", "t1@example.com", "t2@example.com", "t3@ecample.com"));
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
   }
