f8ae4fefe760ed141d50241fcbd64a9666264a3b
Andarkis <Standartis@gmail.com>
2018-07-01 20:52:19 +0300

    Реализована сортировка списков

--- addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/model/GroupData.java
@@ -24,7 +24,7 @@ public class GroupData {
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -47,14 +47,13 @@ public class GroupData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
-            Objects.equals(name, groupData.name);
+    return Objects.equals(name, groupData.name);
   }
 
   @Override
   public int hashCode() {
 
-    return Objects.hash(id, name);
+    return Objects.hash(name);
   }
 
   @Override
@@ -64,4 +63,6 @@ public class GroupData {
             ", name='" + name + '\'' +
             '}';
   }
+
+
 }
