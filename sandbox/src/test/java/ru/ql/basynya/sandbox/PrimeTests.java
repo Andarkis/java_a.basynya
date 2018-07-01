8c3799240b8c026b43b0ac2f680794c472e3fb9a
Andarkis <Standartis@gmail.com>
2018-07-01 14:38:51 +0300

    Создан класс PrimeTests
    Описывает тесты для класса Primes

---
+++ sandbox/src/test/java/ru/ql/basynya/sandbox/PrimeTests.java
@@ -0,0 +1,23 @@
+package ru.ql.basynya.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+  @Test
+  public void testPrimes() {
+    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+  }
+
+  @Test
+  public void testNotPrimes() {
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+  }
+
+  @Test (enabled = false)
+  public void testPrimeLong() {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+}
