6b74b9b6de34bd468fbd807dc4283ff529cfc22a
Andarkis <Standartis@gmail.com>
2018-07-02 02:11:52 +0300

    Добавлен тест для сравнения списков контактов при создании

--- addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.ql.basynya.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -13,8 +14,8 @@ public class ContactCreationTests extends TestBase {
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
     ContactData contact = new ContactData(
-            "John",
-            "Doe",
+            "WJohn",
+            "WDoe",
             "test1",
             "55555",
             "88005553535",
@@ -28,5 +29,11 @@ public class ContactCreationTests extends TestBase {
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(before.size() + 1, after.size());
+
+    before.add(contact);
+    Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(),c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
     }
 }
