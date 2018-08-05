90e304280e0072d5c0ce88ed5ec00b761266e8e9
Andarkis <Standartis@gmail.com>
2018-08-06 00:52:37 +0300

    Лекция 8.7. Реализовано получение почты на внешний почтовый сервер.

--- mantis-tests/src/test/java/ru/ql/basynya/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/ql/basynya/mantis/tests/TestBase.java
@@ -12,16 +12,18 @@ public class TestBase {
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+  private String target = "config/config_inc.php";
+  private String backup = "config/config_inc.php.bak";
 
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
-    app.ftp().upload(new File("src/test/resources/config_inc.php"), "config/config_inc.php", "config/config_inc.php.bak");
+    app.ftp().upload(new File("src/test/resources/config_inc.php"), target, backup);
   }
 
   @AfterSuite (alwaysRun = true)
   public void tearDown() throws IOException {
     app.stop();
-    app.ftp().restore("config/config_inc.php.bak","config/config_inc.php");
+    app.ftp().restore(backup,target);
   }
 }
