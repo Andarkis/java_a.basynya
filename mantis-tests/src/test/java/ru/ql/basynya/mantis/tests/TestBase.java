2bcc5fd330035729609be8df0ceca4ddc8241d54
Andarkis <Standartis@gmail.com>
2018-08-11 16:47:41 +0300

    Задание 19. Реализована интеграция тестов с баг-трекером MantisBT.

--- mantis-tests/src/test/java/ru/ql/basynya/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/ql/basynya/mantis/tests/TestBase.java
@@ -1,12 +1,19 @@
 package ru.ql.basynya.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.ql.basynya.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class TestBase {
 
@@ -19,6 +26,20 @@ public class TestBase {
   public void setUp() throws Exception {
     app.init();
     app.ftp().upload(new File("src/test/resources/config_inc.php"), target, backup);
+  }
+
+  public boolean isIssueOpen(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+    MantisConnectPortType mc = app.soap().getMantisConnect();
+    IssueData issue = mc.mc_issue_get(
+            app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), BigInteger.valueOf(issueId));
+    String status = issue.getStatus().getName();
+    return !(status.equals("resolved") || status.equals("closed"));
+  }
+
+  public void skipIfNotFixed(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
   }
 
   @AfterSuite(alwaysRun = true)
