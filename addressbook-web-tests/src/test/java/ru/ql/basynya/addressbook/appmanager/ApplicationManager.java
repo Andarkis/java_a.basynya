65cf442a90a2b15b414d1ff2d06c3f8b8738f122
Andarkis <Standartis@gmail.com>
2018-08-12 01:26:28 +0300

    Лекция 10.2. Добавлены настройки удалённого запуска браузера (Selenium Server)

--- addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/ql/basynya/addressbook/appmanager/ApplicationManager.java
@@ -1,18 +1,22 @@
 package ru.ql.basynya.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -38,16 +42,22 @@ public class ApplicationManager {
 
     dbHelper = new DbHelper();
 
-    switch (browser) {
-      case BrowserType.FIREFOX:
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        break;
-      case BrowserType.CHROME:
-        wd = new ChromeDriver();
-        break;
-      case BrowserType.IE:
-        wd = new InternetExplorerDriver();
-        break;
+    if ("".equals(properties.getProperty("selenium.server"))) {
+      switch (browser) {
+        case BrowserType.FIREFOX:
+          wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+          break;
+        case BrowserType.CHROME:
+          wd = new ChromeDriver();
+          break;
+        case BrowserType.IE:
+          wd = new InternetExplorerDriver();
+          break;
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
 
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
